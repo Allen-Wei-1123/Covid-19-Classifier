fb5c077921c6e3185cdf151f4d91c0aa03b4e179
cwa188 <cwa188@sfu.ca>
2021-05-02 10:46:53 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React,{Fragment} from 'react'; 
+import ReactDOM from 'react-dom'; 
+import axios from 'axios'
+import  './App.scss'
+class App extends React.Component {
+
+  state = {
+    msg:"",
+    predicted:"",
+    cnt:0
+  }
+
+  handleChange = e=>{
+    this.setState({
+      msg:e.target.value
+    })
+  }
+
+  handlesubmit = e=>{
+      axios.post('http://127.0.0.1:4455/submit',{
+        msg : this.state.msg
+      }).then((response)=>{
+        console.log(response.data)
+        this.setState({
+          predicted:response.data,
+          
+        })
+        this.setState(prev=>{
+          return {cnt:prev.cnt+1}
+        })
+      },(error)=>{
+        console.log(error)
+      })
+
+  }
+
+  
+  
+
+  render(){
+      return (
+     
+        <Fragment>
+          <link rel="preconnect" href="https://fonts.gstatic.com"></link>
+          <link rel="preconnect" href="https://fonts.gstatic.com"></link>
+        <div className="App">
+          <div className="textarea">
+              <h1>
+                Fake Covid-19 News Classifier
+              </h1>
+              <textarea id = "text" onChange={this.handleChange} >
+
+              </textarea>
+              <input id = "submit-btn"type = "submit" value = "Predict" onClick={this.handlesubmit}></input>
+              <div id = "answer">
+                {
+                  this.state.predicted == 1 ? "Real News" : "Fake News"
+                }
+              </div>
+          </div>
+        </div>
+        </Fragment>
+      );
+  }
 }
 
 export default App;
